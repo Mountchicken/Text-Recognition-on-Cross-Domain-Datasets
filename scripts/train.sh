2ac41868cd9255c540d2285ee8cb9430867ca4da
JiangQing <65173622+Mountchicken@users.noreply.github.com>
2021-07-30 16:25:09 +0800

    Delete scripts directory

--- scripts/train.sh
+++
@@ -1,8 +0,0 @@
-CUDA_VISIBLE_DEVICES=0,1 python new_main.py \
-  --synthetic_train_data_dir ../text_recognition_datasets/NIPS2014/NIPS2014 ../text_recognition_datasets/CVPR2016 \
-  --test_data_dir ../text_recognition_datasets/scene_text_benchmarks/IIIT5K_3000/ \
-  --batch_size 512 \
-  --workers 0 \
-  --arch CRNN \
-  --with_lstm \
-  --max_len 25 \
